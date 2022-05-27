31fdaa28adf6a5e90b64e1c1d516b05334a9d644
Sarthak Sharma <sarthaks225@gmail.com>
2022-05-27 20:33:28 +0530

    feature addedd (update,getByCourse,getByContactNumber,getByName,remove)

--- memberutility/member.java
+++ memberutility/member.java
@@ -85,13 +85,13 @@ private static boolean isCourseValid(String course)
     return true;
 }
 
-
+// .....
 
 private static void add(String data[])
 {
  if(data.length!=5) 
  {
-    System.out.println("Usage: 'contact' 'name' 'course' 'fee'");
+    System.out.println("* Usage: 'contact' 'name' 'course' 'fee'");
     return;
  }
  String contact=data[1];
@@ -113,7 +113,7 @@ private static void add(String data[])
  }
 
  try{
- File file=new File("fileName");
+ File file=new File(fileName);
  RandomAccessFile randomAccessFile=new RandomAccessFile(file,"rw");
  String fContact;
  while(randomAccessFile.getFilePointer()<randomAccessFile.length())
@@ -137,15 +137,113 @@ randomAccessFile.writeBytes(course);
 randomAccessFile.writeBytes("\n");
 randomAccessFile.writeBytes(String.valueOf(fee));
 randomAccessFile.writeBytes("\n");
- }catch(IOException ioe)
- {
+randomAccessFile.close();
+}catch(IOException ioe)
+{
     System.out.println(ioe.getMessage());
- }
+}
 System.out.println("Member added");
 }
 
 private static void update(String data[])
-{}
+{
+
+    if(data.length!=5)
+    {
+        System.out.println("* Usage: 'java memberutility update contact newName newCourseName fee'");
+        return;
+    }
+    String contact;
+    String name;
+    String course;
+    String fee;
+    contact=data[1];
+    name=data[2];
+    course=data[3];
+    fee=data[4];
+    if(!isCourseValid(course))
+    {
+        return;
+    }
+
+    try
+    {
+        File file=new File(fileName);
+        RandomAccessFile randomAccessFile= new RandomAccessFile(file,"rw");
+        boolean flag=false;
+        String fContact="";
+        String fName="";
+        String fCourse="";
+        int fFee=0;
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            if(fContact.equalsIgnoreCase(contact))
+            {
+                flag=true;
+                fName=randomAccessFile.readLine();
+                fCourse=randomAccessFile.readLine();
+                fFee=Integer.parseInt(randomAccessFile.readLine());
+                break;
+            }
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+        }
+        if(flag==false)
+        {
+         System.out.printf("Member with contact: (%s) is not added, So cannot be updated\n",contact);
+         randomAccessFile.close();
+         return;
+        }
+
+        System.out.printf("Member with Contact: (%s)  Name: (%s) Course: (%s) Fee: (%d)\nupdated to\n",fContact,fName,fCourse,fFee);
+        System.out.printf("Name: (%s) Course: (%s) Fee: (%s)\n",name,course,fee);
+
+        randomAccessFile.seek(0);
+
+        File tmpFile=new File("tmpMember.data");
+        tmpFile.delete();
+        RandomAccessFile tmpRandomAccessFile=new RandomAccessFile(tmpFile,"rw");
+        
+        String fFees="";
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            fName=randomAccessFile.readLine();
+            fCourse=randomAccessFile.readLine();
+            fFees=randomAccessFile.readLine();
+            
+            if(!fContact.equalsIgnoreCase(contact))
+            {
+            tmpRandomAccessFile.writeBytes(fContact+"\n");
+            tmpRandomAccessFile.writeBytes(fName+"\n");
+            tmpRandomAccessFile.writeBytes(fCourse+"\n");
+            tmpRandomAccessFile.writeBytes(fFees+"\n");
+            }
+            else
+            {
+                tmpRandomAccessFile.writeBytes(contact+"\n");
+                tmpRandomAccessFile.writeBytes(name+"\n");
+                tmpRandomAccessFile.writeBytes(course+"\n");
+                tmpRandomAccessFile.writeBytes(fee+"\n");
+
+            }
+           
+        }
+        randomAccessFile.close();
+        tmpRandomAccessFile.close();
+        file.delete();
+        tmpFile.renameTo(new File(fileName));
+       
+       
+    }catch(IOException ioe)
+    {
+        System.out.println(ioe.getMessage());
+
+    }    
+
+}
 
 private static void getAll(String data[])
 {
@@ -161,7 +259,7 @@ private static void getAll(String data[])
     String name;
     String course;
     int fee;
-    File file=new File("fileName");
+    File file=new File(fileName);
     if(!file.exists()) 
     {
         System.out.println("No members added");
@@ -200,16 +298,265 @@ private static void getAll(String data[])
 }
 
 private static void getByCourse(String data[])
-{}
+{
+    if(data.length!=2)
+    {
+        System.out.println("Invalid Opearation");
+        System.out.println("Usage: java memberutility getByCourse 'course name'");
+        return;
+    }
+    String course;
+    course=data[1];
+    course=course.trim();
+    if(!isCourseValid(course))
+    {
+        return;
+    }
+    try
+    {
+        File file=new File(fileName);
+        if(!file.exists()) 
+        {
+            System.out.println("No members added");
+            return;
+        }
+
+        RandomAccessFile randomAccessFile=new RandomAccessFile(file,"rw");
+        String fContact;
+        String fName;
+        String fCourse;
+        int fFee;
+        int totalFee=0;
+        int totalRegistration=0;
+
+ 
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            fName=randomAccessFile.readLine();
+            fCourse=randomAccessFile.readLine();
+            fFee=Integer.parseInt(randomAccessFile.readLine());
+            if(course.equalsIgnoreCase(fCourse))
+            {
+                System.out.printf("Contact: (%s), Name: (%s) Course: (%s) fee: (%d)\n",fContact,fName,fCourse,fFee);
+                ++totalRegistration;
+                totalFee+=fFee;
+            }
+
+        }
+
+        if(totalRegistration==0)
+        {
+            System.out.printf("No member registered for course : '%s'\n",course);
+            randomAccessFile.close();
+            return;
+        }
+
+        System.out.printf("Total registration in '%s': '%d'\n",course,totalRegistration);
+        System.out.println("Total fee collected for "+course+": "+totalFee);
+        randomAccessFile.close();
+    }catch(IOException ioe)
+    {
+        System.out.println(ioe.getMessage());
+        return;
+    }
+    
+
+}
 
 private static void getByContactNumber(String data[])
-{}
+{
+    if(data.length!=2)
+    {
+        System.out.println("Invalid Opearation");
+        System.out.println("Usage: 'java memberutility gtByContactNumber contact'");
+        return;
+    }
+    String contact=data[1];
+    String fContact="";
+    String fName="";
+    String fCourse="";
+    int fFee=0;
+    int flag=0;
+    try
+    {
+       
+        File file=new File(fileName);
+        RandomAccessFile randomAccessFile=new RandomAccessFile(file,"rw");
+        if(randomAccessFile.length()==0)
+        {
+            randomAccessFile.close();
+            System.out.println("No members added");
+            return;
+        }
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            if(!contact.equalsIgnoreCase(fContact))
+            {
+                randomAccessFile.readLine();
+                randomAccessFile.readLine();
+                randomAccessFile.readLine();
+                continue;
+            }
+            fName=randomAccessFile.readLine();
+                  
+            fCourse=randomAccessFile.readLine();
+            fFee=Integer.parseInt(randomAccessFile.readLine());
+            flag=1;
+            break;
+        }
+        
+        if(flag==0)
+        {
+            System.out.printf("Invalid contact number: '%s'\n",contact);
+            randomAccessFile.close();
+            return;
+        }
+
+        System.out.printf("Contact: (%s), Name: (%s) Course: (%s) fee: (%d)\n",fContact,fName,fCourse,fFee);
+        randomAccessFile.close();
+    }catch(IOException ioe)
+    {
+        System.out.println(ioe.getMessage());
+        return;
+    }
+
+
+
+
+}
 
 private static void getByName(String data[])
-{}
+{
+    if(data.length!=2)
+    {
+        System.out.println("Usage: 'java memberutility getByName name'");
+        return;
+    }
+    String name=data[1];
+    name=name.trim();
+    try
+    {
+        File file=new File(fileName);
+        RandomAccessFile randomAccessFile=new RandomAccessFile(file,"rw");
+        if(randomAccessFile.length()==0)
+        {
+            randomAccessFile.close();
+            System.out.println("No members added");
+            return;
+        }
+        String fContact;
+        String fName;
+        String fCourse;
+        int fFee;
+        boolean flag=false;
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            fName=randomAccessFile.readLine();
+            fCourse=randomAccessFile.readLine();
+            fFee=Integer.parseInt(randomAccessFile.readLine());
+            if(fName.equalsIgnoreCase(name))
+            {
+                System.out.printf("Contact: (%s), Name: (%s) Course: (%s) fee: (%d)\n",fContact,fName,fCourse,fFee);
+                flag=true;
+            }
+        }
+        if(flag==false)
+        {
+            System.out.printf("Member with name: '%s' not exists\n",name);
+
+        }
+
+        randomAccessFile.close();
+    }catch(IOException ioe)
+    {
+        System.out.println(ioe.getMessage());
+        return;
+    }
+
+
+
+}
 
 private static void remove(String data[])
-{}
+{
+    if(data.length!=2)
+    {
+        System.out.println("Usage: 'java memberutility remove contact'");
+        return;
+    }
+    String contact;
+    contact=data[1];
+    try
+    {
+        File file=new File(fileName);
+        RandomAccessFile randomAccessFile= new RandomAccessFile(file,"rw");
+        boolean flag=false;
+        String fContact="";
+        String fName="";
+        String fCourse="";
+        int fFee=0;
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            if(fContact.equalsIgnoreCase(contact))
+            {
+                flag=true;
+                fName=randomAccessFile.readLine();
+                fCourse=randomAccessFile.readLine();
+                fFee=Integer.parseInt(randomAccessFile.readLine());
+                break;
+            }
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+            randomAccessFile.readLine();
+        }
+        if(flag==false)
+        {
+         System.out.printf("Member with contact: (%s) is not added\n",contact);
+         randomAccessFile.close();
+         return;
+        }
+
+        System.out.printf("Member with Contact: (%s)  Name: (%s) Course: (%s) Fee: (%d)\nremoved\n",fContact,fName,fCourse,fFee);
+
+        randomAccessFile.seek(0);
+
+        File tmpFile=new File("tmpMember.data");
+        tmpFile.delete();
+        RandomAccessFile tmpRandomAccessFile=new RandomAccessFile(tmpFile,"rw");
+
+        String fFees="";
+        while(randomAccessFile.getFilePointer()<randomAccessFile.length())
+        {
+            fContact=randomAccessFile.readLine();
+            fName=randomAccessFile.readLine();
+            fCourse=randomAccessFile.readLine();
+            fFees=randomAccessFile.readLine();
+            
+            if(!fContact.equalsIgnoreCase(contact))
+            {
+            tmpRandomAccessFile.writeBytes(fContact+"\n");
+            tmpRandomAccessFile.writeBytes(fName+"\n");
+            tmpRandomAccessFile.writeBytes(fCourse+"\n");
+            tmpRandomAccessFile.writeBytes(fFees+"\n");
+            }
+           
+        }
+        randomAccessFile.close();
+        tmpRandomAccessFile.close();
+        file.delete();
+        tmpFile.renameTo(new File(fileName));
+       
+       
+    }catch(IOException ioe)
+    {
+        System.out.println(ioe.getMessage());
+
+    }    
+}
 
 
 }
